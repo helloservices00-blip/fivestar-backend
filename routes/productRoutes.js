8cf1c1e50f08bb176f46591d6106709e52b4ef63
helloservices00-blip <helloservices00@gmail.com>
2025-11-22 22:07:21 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,21 +1,9 @@
 import express from "express";
-import Product from "../models/Product.js";
+import { createProduct, getVendorProducts } from "../controllers/productController.js";
 
 const router = express.Router();
 
-// GET all products
-router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find()
-      .populate("vendor", "shopName")      // vendor name
-      .populate("module", "name")          // module name
-      .populate("category", "name")        // category name
-      .populate("subcategory", "name");   // subcategory name
-
-    res.json(products);
-  } catch (err) {
-    res.status(500).json({ message: "Error fetching products", error: err });
-  }
-});
+router.post("/", createProduct);
+router.get("/vendor/:vendorId", getVendorProducts);
 
 export default router;
