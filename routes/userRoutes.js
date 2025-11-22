81f38e8555f2222bc453efee9ddc88fd72cfcd5a
helloservices00-blip <helloservices00@gmail.com>
2025-11-22 18:31:51 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,15 @@
+// routes/userRoutes.js
 import express from "express";
 import { protect, admin } from "../middleware/authMiddleware.js";
-import {
-  getUsers,
-  getUserById,
-  updateUser,
-  deleteUser
-} from "../controllers/userController.js";
 
 const router = express.Router();
 
-// Admin get all users
-router.get("/", protect, admin, getUsers);
+router.get("/profile", protect, (req, res) => {
+  res.json(req.user);
+});
 
-// Admin get single user
-router.get("/:id", protect, admin, getUserById);
-
-// Admin update user
-router.put("/:id", protect, admin, updateUser);
-
-// Admin delete user
-router.delete("/:id", protect, admin, deleteUser);
+router.get("/all-users", protect, admin, async (req, res) => {
+  // Admin-only route
+});
 
 export default router;
