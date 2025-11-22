276f6cac0b2a6a6b918a59d62bdf5e1886f6c9a0
helloservices00-blip <helloservices00@gmail.com>
2025-11-22 22:10:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import productRoutes from "./routes/productRoutes.js";
+// Import Routes
+import moduleRoutes from "./routes/moduleRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import subcategoryRoutes from "./routes/subcategoryRoutes.js";
-import moduleRoutes from "./routes/moduleRoutes.js";
 import vendorRoutes from "./routes/vendorRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
+
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -22,10 +25,21 @@ app.use("/api/subcategories", subcategoryRoutes);
 app.use("/api/vendors", vendorRoutes);
 app.use("/api/products", productRoutes);
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+// Root endpoint
+app.get("/", (req, res) => {
+  res.send("Multi-vendor Backend is running");
+});
 
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.log("MongoDB connection error:", err));
+
+// Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
