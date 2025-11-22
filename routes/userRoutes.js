f822ad9f20425016759f14403e39b652561a7674
helloservices00-blip <helloservices00@gmail.com>
2025-11-22 18:24:00 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { protect, admin } from "../middlewares/authMiddleware.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 import {
   getUsers,
   getUserById,
